3c9d8996b895e5a4d0973c4017895d3c11ef6db0
Kurt Zaugg <k.zaugg@promptnet.ch>
2019-07-06 16:19:23 +0200

    Create pages.php

---
+++ Configuration/TCA/pages.php
@@ -0,0 +1,7 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+   'PnBase',
+   'Configuration/PageTS/Mod/WebLayout/BackendLayouts.txt',
+   'Base Page TS'
+);
